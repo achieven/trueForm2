c7e4281bda35eb9aaee37e5a9c16f33f28f562f7
achieven <achievendar@gmail.com>
2019-05-19 01:34:07 +0300

    quiz with photos

--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -6,12 +6,14 @@ export class Question {
     questionTypeId: number;
     options: Option[];
     answered: boolean;
+    info: string;
 
     constructor(data: any) {
         data = data || {};
         this.id = data.id;
         this.name = data.name;
         this.questionTypeId = data.questionTypeId;
+        this.info = data.info;
         this.options = [];
         data.options.forEach(o => {
             this.options.push(new Option(o));
